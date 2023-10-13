3ffb6733160f096ebd4e56f0893c575475d83419
Josuecr156 <jmvcr156@gmail.com>
2023-10-13 17:13:20 -0600

    visual

--- PRESENTACION/FrmMenuPrincipal.Designer.cs
+++ PRESENTACION/FrmMenuPrincipal.Designer.cs
@@ -28,34 +28,153 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenuPrincipal));
+            panelMenu = new Panel();
+            btnregistrocursos = new Button();
+            btnestudiantes = new Button();
+            btnProfesores = new Button();
+            btncursos = new Button();
+            panelLogo = new Panel();
+            pictureBox1 = new PictureBox();
+            panelMenu.SuspendLayout();
+            panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
-            // dataGridView1
+            // panelMenu
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(125, 182);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(568, 150);
-            dataGridView1.TabIndex = 0;
+            panelMenu.BackColor = Color.FromArgb(51, 51, 76);
+            panelMenu.Controls.Add(btnregistrocursos);
+            panelMenu.Controls.Add(btnestudiantes);
+            panelMenu.Controls.Add(btnProfesores);
+            panelMenu.Controls.Add(btncursos);
+            panelMenu.Controls.Add(panelLogo);
+            panelMenu.Dock = DockStyle.Left;
+            panelMenu.Location = new Point(0, 0);
+            panelMenu.Name = "panelMenu";
+            panelMenu.Size = new Size(220, 497);
+            panelMenu.TabIndex = 0;
+            // 
+            // btnregistrocursos
+            // 
+            btnregistrocursos.Dock = DockStyle.Top;
+            btnregistrocursos.FlatAppearance.BorderSize = 0;
+            btnregistrocursos.FlatStyle = FlatStyle.Flat;
+            btnregistrocursos.ForeColor = Color.Gainsboro;
+            btnregistrocursos.Image = (Image)resources.GetObject("btnregistrocursos.Image");
+            btnregistrocursos.ImageAlign = ContentAlignment.MiddleLeft;
+            btnregistrocursos.Location = new Point(0, 279);
+            btnregistrocursos.Name = "btnregistrocursos";
+            btnregistrocursos.Padding = new Padding(12, 0, 0, 0);
+            btnregistrocursos.Size = new Size(220, 60);
+            btnregistrocursos.TabIndex = 4;
+            btnregistrocursos.Text = "  Registro Cursos";
+            btnregistrocursos.TextAlign = ContentAlignment.MiddleLeft;
+            btnregistrocursos.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnregistrocursos.UseVisualStyleBackColor = true;
+            btnregistrocursos.Click += btnregistrocursos_Click;
+            // 
+            // btnestudiantes
+            // 
+            btnestudiantes.Dock = DockStyle.Top;
+            btnestudiantes.FlatAppearance.BorderSize = 0;
+            btnestudiantes.FlatStyle = FlatStyle.Flat;
+            btnestudiantes.ForeColor = Color.Gainsboro;
+            btnestudiantes.Image = (Image)resources.GetObject("btnestudiantes.Image");
+            btnestudiantes.ImageAlign = ContentAlignment.MiddleLeft;
+            btnestudiantes.Location = new Point(0, 219);
+            btnestudiantes.Name = "btnestudiantes";
+            btnestudiantes.Padding = new Padding(12, 0, 0, 0);
+            btnestudiantes.Size = new Size(220, 60);
+            btnestudiantes.TabIndex = 3;
+            btnestudiantes.Text = "  Estudiantes";
+            btnestudiantes.TextAlign = ContentAlignment.MiddleLeft;
+            btnestudiantes.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnestudiantes.UseVisualStyleBackColor = true;
+            btnestudiantes.Click += btnestudiantes_Click;
+            // 
+            // btnProfesores
+            // 
+            btnProfesores.Dock = DockStyle.Top;
+            btnProfesores.FlatAppearance.BorderSize = 0;
+            btnProfesores.FlatStyle = FlatStyle.Flat;
+            btnProfesores.ForeColor = Color.Gainsboro;
+            btnProfesores.Image = (Image)resources.GetObject("btnProfesores.Image");
+            btnProfesores.ImageAlign = ContentAlignment.MiddleLeft;
+            btnProfesores.Location = new Point(0, 159);
+            btnProfesores.Name = "btnProfesores";
+            btnProfesores.Padding = new Padding(12, 0, 0, 0);
+            btnProfesores.Size = new Size(220, 60);
+            btnProfesores.TabIndex = 2;
+            btnProfesores.Text = "  Profesores";
+            btnProfesores.TextAlign = ContentAlignment.MiddleLeft;
+            btnProfesores.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnProfesores.UseVisualStyleBackColor = true;
+            btnProfesores.Click += btnProfesores_Click;
+            // 
+            // btncursos
+            // 
+            btncursos.Dock = DockStyle.Top;
+            btncursos.FlatAppearance.BorderSize = 0;
+            btncursos.FlatStyle = FlatStyle.Flat;
+            btncursos.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btncursos.ForeColor = Color.Gainsboro;
+            btncursos.Image = (Image)resources.GetObject("btncursos.Image");
+            btncursos.ImageAlign = ContentAlignment.MiddleLeft;
+            btncursos.Location = new Point(0, 99);
+            btncursos.Name = "btncursos";
+            btncursos.Padding = new Padding(12, 0, 0, 0);
+            btncursos.Size = new Size(220, 60);
+            btncursos.TabIndex = 1;
+            btncursos.Text = "  Cursos";
+            btncursos.TextAlign = ContentAlignment.MiddleLeft;
+            btncursos.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btncursos.UseVisualStyleBackColor = true;
+            btncursos.Click += btncursos_Click;
+            // 
+            // panelLogo
+            // 
+            panelLogo.BackColor = Color.FromArgb(39, 39, 58);
+            panelLogo.Controls.Add(pictureBox1);
+            panelLogo.Dock = DockStyle.Top;
+            panelLogo.Location = new Point(0, 0);
+            panelLogo.Name = "panelLogo";
+            panelLogo.Size = new Size(220, 99);
+            panelLogo.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(190, 78);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
             // 
             // FrmMenuPrincipal
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(dataGridView1);
+            ClientSize = new Size(800, 497);
+            Controls.Add(panelMenu);
             Name = "FrmMenuPrincipal";
             Text = "Form1";
             Load += FrmMenuPrincipal_Load;
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panelMenu.ResumeLayout(false);
+            panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private Panel panelMenu;
+        private Panel panelLogo;
+        private Button btncursos;
+        private Button btnestudiantes;
+        private Button btnProfesores;
+        private Button btnregistrocursos;
+        private PictureBox pictureBox1;
     }
 }
--- PRESENTACION/FrmMenuPrincipal.cs
+++ PRESENTACION/FrmMenuPrincipal.cs
@@ -7,10 +7,15 @@ namespace PRESENTACION
     public partial class FrmMenuPrincipal : Form
     {
         LogicaMenuPrincipal logicaMenuPrincipal = new LogicaMenuPrincipal();
-
+        //Campos
+        private Button currentButton;
+        private Random random;
+        private int tempIndex;
+        //Constructor
         public FrmMenuPrincipal()
         {
             InitializeComponent();
+            random = new Random();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -20,8 +25,65 @@ namespace PRESENTACION
 
         private void FrmMenuPrincipal_Load(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = null;
-            dataGridView1.DataSource = logicaMenuPrincipal.Test();
+
+        }
+        private Color SeleccionarTemaColor()
+        {
+            int index = random.Next(TemasdeColor.ColorList.Count);
+            while (tempIndex == index)
+            {
+                random.Next(TemasdeColor.ColorList.Count);
+            }
+            tempIndex = index;
+            string color = TemasdeColor.ColorList[index];
+            return ColorTranslator.FromHtml(color);
+        }
+        private void ActivateButton(object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button)btnSender)
+                {
+                    DisableButton();
+                    Color color = SeleccionarTemaColor();
+                    currentButton = (Button)btnSender;
+                    currentButton.BackColor = color;
+                    currentButton.ForeColor = Color.White;
+                }
+
+            }
+        }
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in panelMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(51, 51, 76);
+                    previousBtn.ForeColor = Color.Gainsboro;
+
+                }
+            }
+        }
+
+        private void btncursos_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnProfesores_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnestudiantes_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnregistrocursos_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
         }
     }
 }
